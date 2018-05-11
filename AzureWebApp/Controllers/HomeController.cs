f2dab31d928fc205231150a5826151702182545d
ISDEV069 <ISDEV069@5DYZM62>
2018-05-11 07:27:47 +0200

    Changed controller

--- AzureWebApp/Controllers/HomeController.cs
+++ AzureWebApp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace AzureWebApp.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Test";
 
             return View();
         }
